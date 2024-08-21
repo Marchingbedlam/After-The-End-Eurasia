b031a8a5bc03170cc50182cc65766d0ac386ddbc
Fishedotjpg <56416855+Fishedotjpg@users.noreply.github.com>
2024-08-21 22:41:44 +0100

    Update defines.lua

--- ATE_Eurasia/common/defines.lua
+++ ATE_Eurasia/common/defines.lua
@@ -1507,7 +1507,7 @@ NDefines = {
 	},
 
 	NTechnology = {
-		DONT_EXECUTE_TECH_BEFORE = 769,												-- Set this to your earliest starting year
+		DONT_EXECUTE_TECH_BEFORE = 2666,												-- Set this to your earliest starting year
 		POINTS_PER_ATTRIBUTE = 0.04,
 
 		BASE_NEIGHBOUR_SPREAD_BONUS = 0.03, 										-- bonus for each neighbour with the tech
